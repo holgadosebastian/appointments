5bff207a0ff51efc107f676f7fa5e8fbd8dd364c
holgadosebastian <holgado.sebastian@gmail.com>
2022-11-07 21:20:48 +0100

    Adds .eslintrc.json

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -1,15 +1,17 @@
 import React from "react"
 
-export const AppointmentForm = ({ selectableServices }) => (
-  <form>
-    <select name="service">
-      <option></option>
-      {selectableServices.map(service => (
-        <option key={service}>{service}</option>
-      ))}
-    </select>
-  </form>
-)
+export const AppointmentForm = ({ selectableServices }) => {
+  return (
+    <form>
+      <select name="service">
+        <option></option>
+        {selectableServices.map(service => (
+          <option key={service}>{service}</option>
+        ))}
+      </select>
+    </form>
+  )
+}
 
 AppointmentForm.defaultProps = {
   selectableServices: ["Cut", "Blow-dry", "Cut & color", "Beard trim", "Cut & beard trim", "Extensions"],
