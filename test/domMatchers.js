843f85090ac627fe8a30fb59dc5d6642ed139fb6
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-01-17 17:00:20 +0100

    Adds toNeRenderedWithProps matcher

--- test/domMatchers.js
+++ test/domMatchers.js
@@ -2,5 +2,6 @@ import { toContainText } from "./matchers/toContainText"
 import { toHaveClass } from "./matchers/toHaveClass"
 import { toBeInputFieldOfType } from "./matchers/toBeInputFieldOfType"
 import { toBeElementWithTag } from "./matchers/toBeElementWithTag"
+import { toBeRenderedWithProps } from "./matchers/toBeRenderedWithProps"
 
-expect.extend({ toContainText, toHaveClass, toBeInputFieldOfType, toBeElementWithTag })
+expect.extend({ toContainText, toHaveClass, toBeInputFieldOfType, toBeElementWithTag, toBeRenderedWithProps })
