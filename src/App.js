3f9d97af23729e9dce17bb36d271f32e55f935e0
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-01-18 15:36:27 +0100

    Updates App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useState, useCallback } from "react"
 import { AppointmentsDayViewLoader } from "./components/AppointmentsDayViewLoader"
-import { sampleAppointments } from "./sampleData"
+import { CustomerForm } from "./components/CustomerForm"
 
-export const App = () => <AppointmentsDayViewLoader />
+export const App = () => {
+  const [view, setView] = useState("dayView")
+  const transitionToAddCustomer = useCallback(() => setView("addCustomer"), [])
+
+  return (
+    <>
+      <menu>
+        <li>
+          <button type="button" onClick={transitionToAddCustomer}>
+            Add customer and appointment
+          </button>
+        </li>
+      </menu>
+      {view === "addCustomer" ? <CustomerForm /> : null}
+      <AppointmentsDayViewLoader />
+    </>
+  )
+}
--- test/App.test.js
+++ test/App.test.js
@@ -1,13 +1,20 @@
 import React from "react"
-import { initializeReactContainer, render, element } from "./reactTestExtensions"
+import { initializeReactContainer, render, element, click } from "./reactTestExtensions"
 import { App } from "../src/App"
 import { AppointmentsDayViewLoader } from "../src/components/AppointmentsDayViewLoader"
+import { CustomerForm } from "../src/components/CustomerForm"
 
 jest.mock("../src/components/AppointmentsDayViewLoader", () => ({
   AppointmentsDayViewLoader: jest.fn(() => <div id="AppointmentsDayViewLoader" />),
 }))
 
+jest.mock("../src/components/CustomerForm", () => ({
+  CustomerForm: jest.fn(() => <div id="CustomerForm" />),
+}))
+
 describe("App", () => {
+  const beginAddingCustomerAndAppointment = () => click(element("menu > li > button:first-of-type"))
+
   beforeEach(() => {
     initializeReactContainer()
   })
@@ -15,6 +22,25 @@ describe("App", () => {
   it("initially renders AppointmentsDayViewLoader", () => {
     render(<App />)
 
-    expect(element("#AppointmentsDayViewLoader")).not.toBeNull()
+    expect(AppointmentsDayViewLoader).toBeRenderedWithProps({})
+  })
+
+  it("has a menu bar", () => {
+    render(<App />)
+
+    expect(element("menu")).not.toBeNull()
+  })
+
+  it("has a button to initiate add customer and appointment action", () => {
+    render(<App />)
+    const firstButton = element("menu > li > button:first-of-type")
+
+    expect(firstButton).toContainText("Add customer and appointment")
+  })
+
+  it("displays the CustomerForm when button is clicked", () => {
+    render(<App />)
+    beginAddingCustomerAndAppointment()
+    expect(CustomerForm).toBeRenderedWithProps({})
   })
 })
