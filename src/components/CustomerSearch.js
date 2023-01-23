d916711776d2470a3c748859dc2da93984c9c22c
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-01-23 15:35:50 +0100

    Displays costumer data in CustomerSearch after fetch

--- src/components/CustomerSearch.js
+++ src/components/CustomerSearch.js
@@ -1,14 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-export const CustomerSearch = () => (
-  <table>
-    <thead>
-      <tr>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Phone number</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-  </table>
+const CustomerRow = ({ customer }) => (
+  <tr>
+    <td>{customer.firstName}</td>
+    <td>{customer.lastName}</td>
+    <td>{customer.phoneNumber}</td>
+  </tr>
 )
+
+export const CustomerSearch = () => {
+  const [costumers, setCostumers] = useState([])
+
+  useEffect(() => {
+    const getCustomers = async () => {
+      const result = await global.fetch("/customers", {
+        method: "GET",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+      })
+
+      const data = await result.json()
+      setCostumers(data)
+    }
+
+    getCustomers()
+  }, [])
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {costumers.map(customer => (
+          <CustomerRow customer={customer} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
--- test/CustomerSearch.test.js
+++ test/CustomerSearch.test.js
@@ -1,15 +1,44 @@
 import React from "react"
 import { initializeReactContainer, elements, renderAndWait, textOf } from "./reactTestExtensions"
+import { fetchResponseOk } from "./builders/fetch"
 import { CustomerSearch } from "../src/components/CustomerSearch"
+
+const oneCustomer = [
+  {
+    id: 1,
+    firstName: "A",
+    lastName: "B",
+    phoneNumber: "1",
+  },
+]
 
 describe("CustomerSearch", () => {
   beforeEach(() => {
     initializeReactContainer()
+    jest.spyOn(global, "fetch").mockResolvedValue(fetchResponseOk([]))
   })
 
   it("renders a table with four headings", async () => {
     await renderAndWait(<CustomerSearch />)
     const headings = elements("table th")
     expect(textOf(headings)).toEqual(["First name", "Last name", "Phone number", "Actions"])
+  })
+
+  it("fetches all customer data when component mounts", async () => {
+    await renderAndWait(<CustomerSearch />)
+    expect(global.fetch).toBeCalledWith("/customers", {
+      method: "GET",
+      credentials: "same-origin",
+      headers: { "Content-Type": "application/json" },
+    })
+  })
+
+  it("renders all customer data in a table row", async () => {
+    global.fetch.mockResolvedValue(fetchResponseOk(oneCustomer))
+    await renderAndWait(<CustomerSearch />)
+    const columns = elements("table > tbody > tr > td")
+    expect(columns[0]).toContainText("A")
+    expect(columns[1]).toContainText("B")
+    expect(columns[2]).toContainText("1")
   })
 })
