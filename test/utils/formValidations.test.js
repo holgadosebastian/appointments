6ef73f3f999e6459743464d0d48468ac5080fdff
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-01-23 15:05:23 +0100

    Adds tests for formValidations required method

--- test/utils/formValidations.test.js
+++ test/utils/formValidations.test.js
@@ -1,18 +1,29 @@
 import { required, match, list, validateMany, anyErrors, hasError } from "../../src/utils/formValidations"
 
 describe("formValidations", () => {
-  const validators = {
-    firstName: required("First name is required"),
-    lastName: required("Last name is required"),
-    phoneNumber: list(
-      required("Phone number is required"),
-      match(/^[0-9+()\- ]*$/, "Only numbers, spaces and the symbols are allowed: ( ) + -")
-    ),
-  }
-
   const validationErrors = {
     phoneNumber: "Phone must contain numbers",
   }
+
+  describe("required", () => {
+    it("returns a callback function", () => {
+      const result = required("First name is required")
+      console.log("result", result)
+      expect(typeof result).toBe("function")
+    })
+
+    describe("passing an argument to callback function", () => {
+      it("returns description text if value is empty", () => {
+        const result = required("First name is required")("")
+        expect(result).toEqual("First name is required")
+      })
+
+      it("returns description text if value is empty", () => {
+        const result = required("First name is required")(" ")
+        expect(result).toEqual("First name is required")
+      })
+    })
+  })
 
   describe("hasError", () => {
     it("returns true if validation for fieldName is not empty", () => {
