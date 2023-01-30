ccc6b37b12f45ddeb75540a2dcd33e6096a368cb
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-01-30 17:04:31 +0100

    Adds redux store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import "./styles/index.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { configureStore } from "./store"
 
 import { App } from "./App"
 
 const root = document.createElement("root")
 document.body.appendChild(root)
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
